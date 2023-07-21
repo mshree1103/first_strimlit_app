8c4117c91443c310f40a2ad99db58f5414443d37
mshree1103 <140066006+mshree1103@users.noreply.github.com>
2023-07-21 18:14:13 +0530

    Update firststreamlit_app.py

--- firststreamlit_app.py
+++ firststreamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
 import pandas
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 streamlit.title('happy')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')

d7c2ead2b19652d5566ee806d7d84a57589c2684
mshree1103 <140066006+mshree1103@users.noreply.github.com>
2023-07-21 18:46:20 +0530

    Update firststreamlit_app.py

--- firststreamlit_app.py
+++ firststreamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 
 import requests
+import snowflake.connector
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 streamlit.header("Fruityvice Fruit Advice!")
